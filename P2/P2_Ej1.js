0ded17eeb312c0367f658df9ff02d3ca10a047de
Carolina Cordoba <carolinacs.mar@gmail.com>
2023-03-24 17:50:15 -0500

    Objetos y ciclos

---
+++ P2/P2_Ej1.js
@@ -0,0 +1,5 @@
+/*1. Iterar por todos los elementos del array hasta que llegue al día jueves, imprimir por consola
+*`llegamos al dia ${[nombreDelItem]}` y después salir del loop.
+*/
+
+const weekDays=['lunes','martes','miercoles','jueves','viernes'];
